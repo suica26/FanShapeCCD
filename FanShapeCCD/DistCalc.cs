2adf5f71832997b5a23161862575ebc6c3e2c9cd
suica26 <m011931038@edu.teu.ac.jp>
2022-11-13 20:23:31 +0900

    最短距離計算関数移行完了

---
+++ FanShapeCCD/DistCalc.cs
@@ -0,0 +1,94 @@
+﻿using FK_CLI;
+using System;
+
+namespace FanShapeCCD
+{
+    static public class DistCalc
+    {
+        static public double CalcPointLineDist(fk_Vector P, fk_Vector LP, fk_Vector V, ref fk_Vector H, ref double t)
+        {
+            //媒介変数の計算
+            t = V * V / (V * LP - V * P);
+            //直線状の点
+            H = LP + V * t;
+            //最短距離の計算
+            return (H - P).Dist();
+        }
+
+        static public double CalcPointSegmentDist(fk_Vector P, fk_Vector S, fk_Vector E, ref fk_Vector H, ref double t)
+        {
+            //線分の方向ベクトル
+            var V = E - S;
+            double d = CalcPointLineDist(P, S, V, ref H, ref t);
+
+            //始点よりも外側にある場合
+            if(t < 0.0)
+            {
+                d = (P - S).Dist();
+                H = S;
+                t = 0.0;
+            }
+            else if(t > 1.0) //終点よりも外側にある場合
+            {
+                d = (P - E).Dist();
+                H = E;
+                t = 1.0;
+            }
+
+            return d;
+        }
+
+        static public double CalcLineLineDist(fk_Vector A, fk_Vector V, fk_Vector B, fk_Vector W, ref fk_Vector P, ref fk_Vector Q, ref double s, ref double t)
+        {
+            double VV = V * V;
+            double VW = V * W;
+            double WW = W * W;
+            var AB = B - A;
+
+            double tDeno = WW * VV - VW * VW;
+            double tNume = V * AB * VW - AB * W * VV;
+            t = tNume / tDeno;
+
+            Q = B + W * t;
+
+            s = V * (AB + W * t) / VV;
+
+            P = A + V * s;
+
+            return (P - Q).Dist();
+        }
+
+        static public double CalcSegmentSegmentDist(fk_Vector A, fk_Vector B, fk_Vector C, fk_Vector D, fk_Vector P, fk_Vector Q, ref double s, ref double t)
+        {
+            //線分ABの方向ベクトル
+            fk_Vector V = B - A;
+            //線分CDの方向ベクトル
+            fk_Vector W = D - C;
+            //最短距離を算出
+            double d = CalcLineLineDist(A, V, C, W, ref P, ref Q, ref s, ref t);
+
+            //最短点がどちらの線分上にも存在する場合には計算終了
+            if((0.0 <= s && s <= 1.0) && (0.0 <= t && t <= 1.0)){
+                return d;
+            }
+
+            //垂線の足が外にある事が判明
+            //sを0～1の間にクランプして線分CDに垂線を降ろす
+            s = Math.Clamp(s, 0.0, 1.0);
+            P = A + V * s; //点Pを計算
+            d = CalcPointLineDist(P, C, W, ref Q, ref t);    //最短距離を計算しなおし
+            if (0.0 <= t && t <= 1.0) return d;
+
+            //tを0～1の間にクランプして線分ABに垂線を降ろす
+            t = Math.Clamp(t, 0.0, 1.0);
+            Q = C + W * t; //点Qを計算
+            d = CalcPointLineDist(Q, A, V, ref P, ref s);    //最短距離を計算しなおし
+            if (0.0 <= s && s <= 1.0) return d;
+
+            // 双方の端点が最短と判明
+            s = Math.Clamp(s, 0.0, 1.0);
+            P = A + V * s;
+            return d;
+        }
+    }
+}
---
+++ FanShapeCCD/main.cs
@@ -0,0 +1,70 @@
+﻿using FanShapeCCD;
+using FK_CLI;
+using System;
+using System.Collections.Generic;
+
+//ウィンドウ変数
+fk_AppWindow win = new fk_AppWindow();
+
+var models = new List<fk_Model>();
+var sphere = new fk_Sphere(8, 1.0);
+
+for(int i = 0; i < 6; i++)
+{
+    var model = new fk_Model();
+    model.Shape = sphere;
+    models.Add(model);
+}
+
+fk_Vector A, B, C, D;
+A = new fk_Vector(-10.0, -10.0, 0.0);
+B = new fk_Vector(10.0, 10.0, 0.0);
+C = new fk_Vector(-10.0, 10.0, 0.0);
+D = new fk_Vector(10.0, -10.0, 0.0);
+
+var AB = new fk_Line();
+AB.SetVertex(0, A);
+AB.SetVertex(1, B);
+var CD = new fk_Line();
+CD.SetVertex(0, C);
+CD.SetVertex(1, D);
+
+var lAB = new fk_Model();
+lAB.Shape = AB;
+lAB.Material = fk_Material.AshGray;
+var lCD = new fk_Model();
+lCD.Shape = CD;
+lCD.Material = fk_Material.AshGray;
+
+models[0].GlMoveTo(A);
+models[1].GlMoveTo(B);
+models[2].GlMoveTo(C);
+models[3].GlMoveTo(D);
+
+for(int i=0;i<4;i++)
+{
+    win.Entry(models[i]);
+}
+
+WindowSetup();
+
+win.Entry(lAB);
+win.Entry(lCD);
+
+while (win.Update() == true)
+{
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+
+//ウィンドウ設定
+void WindowSetup()
+{
+    win.CameraPos = new fk_Vector(0.0, 0.0, 100.0);
+    win.CameraFocus = new fk_Vector(0.0, 0.0, 0.0);
+    win.Size = new fk_Dimension(1000, 800);
+    win.ShowGuide(fk_Guide.GRID_XY);
+    win.BGColor = new fk_Color(0.6, 0.7, 0.8);
+    win.TrackBallMode = true;
+    win.Open();
+}
